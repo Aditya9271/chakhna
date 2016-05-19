01db805188a2d63ae50195c83c6d4ca7d3c10e1e
Aditya Aman <reach2aditya2@gmail.com>
2016-05-19 14:49:46 +0530

    commit to master
    first commit

---
+++ config/secret.js
@@ -0,0 +1,21 @@
+module.exports = {
+
+    'facebookAuth' : {
+        'clientID'      : '807865052647602', // your App ID
+        'clientSecret'  : 'a64e22a3aef302c90b8da1501cf68ba3', // your App Secret
+        'callbackURL'   : 'http://localhost:8080/auth/facebook/callback'
+    },
+
+    'twitterAuth' : {
+        'consumerKey'       : 'your-consumer-key-here',
+        'consumerSecret'    : 'your-client-secret-here',
+        'callbackURL'       : 'http://localhost:8080/auth/twitter/callback'
+    },
+
+    'googleAuth' : {
+        'clientID'      : '783226425096-9ts19potmjuc7aiioit5b3fhtueplikn.apps.googleusercontent.com',
+        'clientSecret'  : 'SL4Q6IOnF-KdrCUHSggkwO-Y',
+        'callbackURL'   : 'http://localhost:8080/auth/google/callback'
+    }
+
+};
